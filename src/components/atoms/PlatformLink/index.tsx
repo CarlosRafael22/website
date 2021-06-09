a3d664d9fb460ddb9e7428c84e2654ee0b79e800
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 18:18:35 -0300

    feat(PlatformLink): Add anchor and icons to component

--- src/components/atoms/PlatformLink/index.tsx
+++ src/components/atoms/PlatformLink/index.tsx
@@ -1,32 +1,39 @@
-import { Flex, Icon } from '@chakra-ui/react'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { Flex, Link } from '@chakra-ui/react'
+import { faLinkedin, faTwitter, faDev, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-type IconsOptions = 'linkedin'
+type IconsOptions = 'linkedin' | 'dev' | 'twitter' | 'github' | 'email'
 
 export type PlatformLinkProps =  {
-    platform: IconsOptions
+    platform: IconsOptions,
+    link: string
 }
 
 const getIcon = (option: IconsOptions) => {
     return {
-        'linkedin': 'fa-linkedin'
+        'linkedin': faLinkedin,
+        'dev': faDev,
+        'twitter': faTwitter,
+        'github': faGithub,
+        'email': faEnvelope
     }[option]
 }
 
-const PlatformLink = ({ platform }: PlatformLinkProps) => {
-    const iconFa = getIcon(platform)
+const PlatformLink = ({ platform, link }: PlatformLinkProps) => {
+    const faIcon = getIcon(platform)
     return (
-        <Flex
-            h='60px' w='60px'
-            backgroundColor='white'
-            borderRadius='50%'
-            justifyContent='center' alignItems='center'
-            _hover={{ boxShadow: '0 0 0 5px #EEEEEE' }}
+        <Link href={link} isExternal>
+            <Flex
+                h='60px' w='60px'
+                backgroundColor='white'
+                borderRadius='50%'
+                justifyContent='center' alignItems='center'
+                _hover={{ boxShadow: '0 0 0 5px #EEEEEE' }}
             >
-            <FontAwesomeIcon icon={faLinkedin} size='2x' />
-        </Flex>
+                <FontAwesomeIcon icon={faIcon} size='2x' />
+            </Flex>
+        </Link>
     )
 }
 
