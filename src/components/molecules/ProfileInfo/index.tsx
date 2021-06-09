f419f428177db2cf4fca53b8f06985d72aadb164
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 19:36:57 -0300

    feat(ProfileInfo): Resize according to viewport

--- src/components/molecules/ProfileInfo/index.tsx
+++ src/components/molecules/ProfileInfo/index.tsx
@@ -1,9 +1,11 @@
-import { Avatar, Heading, Text, Flex } from '@chakra-ui/react'
+import { Avatar, Heading, Text, Flex, useBreakpointValue } from '@chakra-ui/react'
 
 const ProfileHeader = () => {
+    const avatarSize = useBreakpointValue({ base: 'xl', md: '2xl' })
+
     return (
-        <Flex flexDirection='column' h='200px' w='400px' justifyContent='space-between' alignItems='center'>
-            <Avatar size="2xl" name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+        <Flex flexDirection='column' h={['180px','200px']} w={['300px','400px']} justifyContent='space-between' alignItems='center'>
+            <Avatar size={avatarSize} name='Rafael Leitao' src="https://bit.ly/kent-c-dodds" />
             <Heading variant='h3'>Frontend Software Engineer</Heading>
             <Text variant='body1'>Working @Holidu</Text>
         </Flex>
