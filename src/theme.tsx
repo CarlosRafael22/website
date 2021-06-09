e20c4ef8ba54f9ddbb0244510dd1d6677e76e156
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 13:55:03 -0300

    feat(theme): Add Montserrat and Heading/Text variants

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,7 +1,7 @@
 import { extendTheme } from '@chakra-ui/react'
 import { createBreakpoints } from '@chakra-ui/theme-tools'
 
-const fonts = { mono: `'Menlo', monospace` }
+// const fonts = { mono: `'Menlo', monospace` }
 
 const breakpoints = createBreakpoints({
   sm: '40em',
@@ -14,8 +14,42 @@ const theme = extendTheme({
   colors: {
     black: '#16161D',
   },
-  fonts,
+  fonts: {
+    heading: 'Montserrat',
+    body: 'Montserrat'
+  },
   breakpoints,
+  space: {
+    '3xl': '12rem'
+  },
+  components: {
+    Heading: {
+      variants: {
+        h1: {
+          fontSize: '4xl', fontWeight: 'bold'
+        },
+        h2: {
+            fontSize: '3xl', fontWeight: 'bold'
+        },
+        h3: {
+            fontSize: 'xl', fontWeight: 'bold'
+        },
+        h4: {
+            fontSize: 'md', fontWeight: 'bold'
+        }
+      }
+    },
+    Text: {
+      variants: {
+        body1: {
+          fontSize: 'md', fontWeight: '400'
+        },
+        body2: {
+          fontSize: 'md', fontWeight: '300'
+        }
+      }
+    }
+  }
 })
 
 export default theme
