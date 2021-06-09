f7fb018c09890303f80bfaca5b6a6417b4eb2ad7
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 19:29:29 -0300

    feat(PlatformLink): Resize according to viewport

--- src/components/atoms/PlatformLink/index.tsx
+++ src/components/atoms/PlatformLink/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex, Link, useBreakpointValue } from '@chakra-ui/react'
+import { SizeProp } from '@fortawesome/fontawesome-svg-core'
 import { faLinkedin, faTwitter, faDev, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -22,16 +23,18 @@ const getIcon = (option: IconsOptions) => {
 
 const PlatformLink = ({ platform, link }: PlatformLinkProps) => {
     const faIcon = getIcon(platform)
+    const faSize = useBreakpointValue({ base: '2x', md: '2x' })
+
     return (
         <Link href={link} isExternal>
             <Flex
-                h='60px' w='60px'
+                h={['50px','60px']} w={['50px','60px']}
                 backgroundColor='white'
                 borderRadius='50%'
                 justifyContent='center' alignItems='center'
                 _hover={{ boxShadow: '0 0 0 5px #EEEEEE' }}
             >
-                <FontAwesomeIcon icon={faIcon} size='2x' />
+                <FontAwesomeIcon icon={faIcon} size={faSize as SizeProp} />
             </Flex>
         </Link>
     )
