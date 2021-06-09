ac13397ae78dbe8695fa11fe467a9a9c48424869
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 18:19:12 -0300

    feat(PlatformLink): Add options argTypes to story

--- src/components/atoms/PlatformLink/stories.tsx
+++ src/components/atoms/PlatformLink/stories.tsx
@@ -3,15 +3,23 @@ import { Story, Meta } from '@storybook/react'
 
 export default {
     title: 'PlatformLink',
-    component: PlatformLink
+    component: PlatformLink,
+    argTypes: {
+        platform: {
+          options: ['linkedin', 'dev', 'twitter', 'github', 'email'],
+          control: { type: 'radio' }
+        }
+      }
 } as Meta
 
 const Template: Story<PlatformLinkProps> = (args) => <PlatformLink {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-    platform: 'linkedin'
+    platform: 'linkedin',
+    link: 'https://dev.to/carlosrafael22'
 }
+
 Default.parameters = {
     backgrounds: {
         default: 'default',
