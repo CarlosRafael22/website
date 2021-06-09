553d3ca0cd1047342281cafce49fb3a7fcbaf72c
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 20:38:17 -0300

    chore(ProfileInfo): Rename Text variant to b1

--- src/components/molecules/ProfileInfo/index.tsx
+++ src/components/molecules/ProfileInfo/index.tsx
@@ -7,7 +7,7 @@ const ProfileHeader = () => {
         <Flex flexDirection='column' h={['180px','200px']} w={['300px','400px']} justifyContent='space-between' alignItems='center'>
             <Avatar size={avatarSize} name='Rafael Leitao' src="https://bit.ly/kent-c-dodds" />
             <Heading variant='h3'>Frontend Software Engineer</Heading>
-            <Text variant='body1'>Working @Holidu</Text>
+            <Text variant='b1'>Working @Holidu</Text>
         </Flex>
     )
 }
