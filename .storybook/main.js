39081cca0ac5a42ee26868792512d0aa55811671
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 13:55:42 -0300

    feat(.storybook): Adapt config files to work with Chakra

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,27 @@
+const path = require("path");
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/**/stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
-}
+  ],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,28 @@
+import React from "react"
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '../src/theme'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// export const decorators = [
+//   (Story) => (
+//       <ChakraProvider resetCSS theme={theme}>
+//         <Story />
+//       </ChakraProvider>
+//   ),
+// ]
+
+const withChakra = (StoryFn) => {
+
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <div id="story-wrapper" style={{ minHeight: "100vh" }}>
+        <StoryFn />
+      </div>
+    </ChakraProvider>
+  )
+}
+
+export const decorators = [withChakra]
