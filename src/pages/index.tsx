7f9f1628995b8a86fa86f2ecc55530ac80190bb6
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 21:52:18 -0300

    chore(pages): Comment not used imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+// import {
+//   Link as ChakraLink,
+//   Text,
+//   Code,
+//   List,
+//   ListIcon,
+//   ListItem,
+// } from '@chakra-ui/react'
+// import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 
-import { Hero } from '../components/Hero'
+// import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+// import { Main } from '../components/Main'
+// import { DarkModeSwitch } from '../components/DarkModeSwitch'
+// import { CTA } from '../components/CTA'
+// import { Footer } from '../components/Footer'
 import Home from '../components/templates/Home'
 
 const Index = () => (
