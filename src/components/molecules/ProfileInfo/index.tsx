16e2ed1948036c224363063bd6f46d024b689b06
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-09 21:46:23 -0300

    style(ProfileInfo): Add real photo + textAlign

--- src/components/molecules/ProfileInfo/index.tsx
+++ src/components/molecules/ProfileInfo/index.tsx
@@ -5,8 +5,8 @@ const ProfileHeader = () => {
 
     return (
         <Flex flexDirection='column' h={['180px','200px']} w={['300px','400px']} justifyContent='space-between' alignItems='center'>
-            <Avatar size={avatarSize} name='Rafael Leitao' src="https://bit.ly/kent-c-dodds" />
-            <Heading variant='h3'>Frontend Software Engineer</Heading>
+            <Avatar size={avatarSize} name='Rafael Leitão' src='https://pbs.twimg.com/profile_images/1394400955127504901/7jm6GeHf_400x400.jpg' />
+            <Heading variant='h3' textAlign='center'>Frontend Software Engineer</Heading>
             <Text variant='b1'>Working @Holidu</Text>
         </Flex>
     )
