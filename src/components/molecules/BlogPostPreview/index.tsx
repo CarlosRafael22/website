d32343405d78e71f245de2bf3548f7cd5ad7684b
Carlos Rafael Leitão <carlosrafael093@gmail.com>
2021-06-14 19:52:30 -0300

    style(BlogPostPreview): Change padding + border

--- src/components/molecules/BlogPostPreview/index.tsx
+++ src/components/molecules/BlogPostPreview/index.tsx
@@ -14,7 +14,7 @@ const BlogPostPreview = ({ title, excerpt, date, slug, tags }: BlogPostPreviewPr
     return (
         <Link href={`/blog/${slug}`}>
             <a>
-            <Box w='500px' borderBottom='1px solid #FFFFFF' paddingBottom='1rem'>
+            <Box w='500px' borderBottom='2px solid #FFFFFF' paddingY='1.5rem'>
                 <Heading variant='h3' marginBottom='0.5rem'>{title}</Heading>
                 <Text variant='b3' marginBottom='0.5rem'>{excerpt}</Text>
                 <Flex>
