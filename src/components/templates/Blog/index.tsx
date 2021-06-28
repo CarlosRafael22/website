3134285a0aee40544f2a8638232deb9ec716fcf7
Rafael Leitão <carlosrafael093@gmail.com>
2021-06-28 18:14:18 -0300

    refactor(Blog): Use Wrapper

--- src/components/templates/Blog/index.tsx
+++ src/components/templates/Blog/index.tsx
@@ -1,17 +1,11 @@
-import { Box, Flex } from '@chakra-ui/react'
-import Header from '../../organisms/Header'
 import BlogPostPreviewSection, { BlogPostPreviewSectionProps } from '../../organisms/BlogPostPreviewSection'
-import Footer from '../../organisms/Footer'
+import Wrapper from '../Wrapper'
 
 const Blog = ({ posts }: BlogPostPreviewSectionProps) => {
     return (
-        <Box>
-            <Header />
-            <Flex w='100vw' bgColor='bgGray' justifyContent='center' alignItems='center'>
-                <BlogPostPreviewSection posts={posts} />
-            </Flex>
-            <Footer />
-        </Box>
+        <Wrapper>
+            <BlogPostPreviewSection posts={posts} />
+        </Wrapper>
     )
 }
 
