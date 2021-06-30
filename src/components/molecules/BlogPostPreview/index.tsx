c4176eed70202821677ab6f21a854768405253eb
Rafael Leitão <carlosrafael093@gmail.com>
2021-06-30 20:21:45 -0300

    style(BlogPostPreview): Add width for mobile

--- src/components/molecules/BlogPostPreview/index.tsx
+++ src/components/molecules/BlogPostPreview/index.tsx
@@ -15,7 +15,7 @@ const BlogPostPreview = ({ title, excerpt, date, slug, tags }: BlogPostPreviewPr
     return (
         <Link href={`/posts/${slug}`}>
             <a>
-            <Box w='600px' paddingY='1.5rem'>
+            <Box w={['100%', '600px']} paddingY='1.5rem'>
                 <Heading variant='h3' marginBottom='0.5rem'>{title}</Heading>
                 <Text variant='b3' marginBottom='0.5rem'>{excerpt}</Text>
                 <BlogPostMetaData tags={tags} date={date} type='postPreview' />
