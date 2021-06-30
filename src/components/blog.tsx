71fc9f4fa75c686f0465a4be9ecf1b749de8662b
Rafael Leitão <carlosrafael093@gmail.com>
2021-06-30 19:12:47 -0300

    feat(components/blog): Add some more custom components

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -1,7 +1,10 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Heading, Text, Link } from '@chakra-ui/react'
 
 export const components = {
-  h1: (props) => <Heading style={{ color: 'red' }} {...props} />,
+  h1: (props) => <Heading variant='h1' {...props} />,
+  h2: (props) => <Heading variant='h2' {...props} />,
+  h3: (props) => <Heading variant='h3' {...props} />,
+  a: props => <Link variant='default' isExternal {...props} />,
   p: (props) => {
     //   console.log('CHILDREN PROPS: ', props.children.mdxType, props.children?.props?.mdxType, props.mdxType, props.parentName)
       return (<Text variant={props.children?.props?.mdxType === 'inlineCode' ? 'code' : 'b2'} {...props} />)
@@ -16,6 +19,8 @@ export const components = {
   inlineCode: props => {
       return (<code style={{ backgroundColor: '#08090a', color: 'white' }} {...props} />)
   },
+  ol: props => <ol style={{ padding: '0 1rem 0 1rem' }} {...props} />,
+  ul: props => <ul style={{ padding: '0 1rem 0 1rem' }} {...props} />,
   CodeWrapper: () => {
     return (
         <Text>Hello everybody</Text>
